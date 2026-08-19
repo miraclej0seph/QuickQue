8663eea57c8bbd25b5d92f0de33f6e6c6f00de49
JasmineBarber-Gray <jasminebarbergray@gmail.com>
2026-08-20 10:50:53 +1200

    created cafe picker

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,5 +20,17 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        // This method returns the service time based on the selected cafe option.
+        private double GetCafeServiceTime()
+        {
+            return CafePicker.SelectedIndex switch
+            {
+                0 => 2.0,
+                1 => 3.0,
+                2 => 1.5,
+                _ => 0
+            };
+        }
     }
 }
