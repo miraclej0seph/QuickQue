1395d01a6d665e5c499af5a38aef4968c31eb7fc
JasmineBarber-Gray <jasminebarbergray@gmail.com>
2026-08-20 12:25:17 +1200

    created image switch depending on selected cafe

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,18 +9,6 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-
         // This method returns the service time based on the selected cafe option.
         private double GetCafeServiceTime()
         {
@@ -31,6 +19,46 @@
                 2 => 1.5,
                 _ => 0
             };
+        }
+
+        // This method returns a recommendation based on the number of people waiting.
+        private string GetRecommendation(int peopleWaiting)
+        {
+            if (peopleWaiting <= 3)
+            {
+                return "Great time to grab something!";
+            }
+            else if (peopleWaiting <= 7)
+            {
+                return "The wait should be manageable.";
+            }
+            else if (peopleWaiting <= 10)
+            {
+                return "You may want to come back later.";
+            }
+            else
+            {
+                return "Consider trying another café.";
+            }
+        }
+
+        // This method changes the image displayed based on the selected cafe option.
+        private void CafePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (CafePicker.SelectedIndex)
+            {
+                case 0:
+                    CafeImage.Source = "maincafe.jpg";
+                    break;
+
+                case 1:
+                    CafeImage.Source = "librarycafe.png";
+                    break;
+
+                case 2:
+                    CafeImage.Source = "studenthubcafe.jpg";
+                    break;
+            }
         }
     }
 }
